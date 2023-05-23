15b81020fe6671d3bc44ce8ffbb9b6abc8b0edab
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-05-23 18:48:14 +0200

    Creating the users , roles and user_roles models

---
+++ src/main/java/com/example/springsecurityauthenticationpostgresql/models/ERole.java
@@ -0,0 +1,7 @@
+package com.example.springsecurityauthenticationpostgresql.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ test2db.sql
@@ -0,0 +1,35 @@
+-- INSERT IGNORE INTO roles(name) VALUES('ROLE_USER');
+-- INSERT IGNORE INTO roles(name) VALUES('ROLE_ADMIN');
+-- INSERT IGNORE INTO roles(name) VALUES('ROLE_MODERATOR');
+
+CREATE TYPE e_role AS ENUM ('role_user','role_moderator','role_admin');
+
+CREATE TABLE roles(
+id integer primary key,
+name e_role length=20);
+
+CREATE TABLE users(
+id integer primary key,
+username varchar(20),
+email varchar(50),
+password varchar(120)
+);
+
+CREATE TABLE user_roles(
+role_id integer  not null,
+user_id bigint not null,
+CONSTRAINT FK_ROLE_ID FOREIGN KEY (role_id)
+      REFERENCES roles (id),
+CONSTRAINT FK_USER_ID FOREIGN KEY (user_id)
+      REFERENCES users (id)
+);
+
+SELECT * FROM roles;
+SELECT * FROM users;
+SELECT * FROM user_roles;
+
+INSERT INTO users
+VALUES(1,'Iryna','iryna@gmail.com','12345'),
+(2,'Irina','irina@gmail.com','23456');
+
+-- DROP table users_roles;
