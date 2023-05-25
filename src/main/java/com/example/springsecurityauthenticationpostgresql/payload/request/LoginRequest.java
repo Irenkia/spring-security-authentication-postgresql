02f4643c82fcbe787b2000a2d7e2301df19f7ea3
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-05-25 11:02:20 +0200

    created Requests

---
+++ src/main/java/com/example/springsecurityauthenticationpostgresql/payload/request/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.example.springsecurityauthenticationpostgresql.payload.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+
+}
